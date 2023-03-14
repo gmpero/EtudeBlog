78d712617281e46b3a328853029fffec56bea139
gmpero <rostislavpen@gmail.com>
2023-03-14 21:49:03 +0300

    add 404

--- 404.php
+++ 404.php
@@ -4,10 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- My Style -->
+    <link rel="stylesheet" href="assets/css/404.css">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>404</title>
 </head>
 <body>
-    <h2>Хехе</h2>
-    <p>Страница не найдена</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <div class="text-center">
+                    <h2 class="name">404</h2>
+                    <p>Ошибка<br>К сожалению запрашиваемая<br> Вами страница не найдена...</p>
+                    <a href="index.php" class="bt-link">Вернуться на главную</a>
+                </div>
+            </div>
+            <div class="col-6 image-flex">
+                <img src="assets/images/404.jpg" class="img">
+            </div>
+        </div>
+    </div>
 </body>
 </html>
