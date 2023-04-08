40667f0ac413eaf5af01640701a720fa6015f307
gmpero <rostislavpen@gmail.com>
2023-04-08 12:20:36 +0300

    up

--- app/controllers/users.php
+++ app/controllers/users.php
@@ -32,7 +32,15 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
             ];
 
             $id = insert('users', $post);
-            $statusMsg = "Пользователь $login успешно зарегистрирован!";
+            $user = selectOne('users', ['id' => $id]);
+            $_SESSION['id'] = $user['id'];
+            $_SESSION['login'] = $user['username'];
+            $_SESSION['admin'] = $user['admin'];
+            if($_SESSION['admin']){
+                header('location:' . BASE_URL . "admin/admin.php");
+            }else{
+                header('location:' . BASE_URL);
+            }
         }
         
     }
